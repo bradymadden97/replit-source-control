d09052fd0f32df58b51c2dffd1d176f6190bc6ae
Brady Madden <bradymadden97@gmail.com>
2022-12-23 18:15:35 -0500

    another temp commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial_new_main_toooo(n):
+def factorial_new_main_tooood(n):
     if n == 0:
         return 1
     else:
-        return n * factorial_new_main_toooo(n - 1)
+        return n * factorial_new_main_tooood(n - 1)
 
 
-print(factorial_new_main_toooo(5))
+print(factorial_new_main_tooood(5))
