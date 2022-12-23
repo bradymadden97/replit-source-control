bad0fce3e73fd57a9009b94246a3fed4e572a327
Brady Madden <bradymadden97@gmail.com>
2022-12-23 18:16:21 -0500

    Temporary Commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial_new_main_tooood(n):
+def factorial_new_main_tobranchone(n):
     if n == 0:
         return 1
     else:
-        return n * factorial_new_main_tooood(n - 1)
+        return n * factorial_new_main_tobranchone(n - 1)
 
 
-print(factorial_new_main_tooood(5))
+print(factorial_new_main_tobranchone(5))
