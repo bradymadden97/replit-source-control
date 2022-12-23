8b94942ff2fd4bee066c66fd2348b520f7318ea1
BradyMadden <>
2022-12-23 21:18:25 +0000

    on new branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def factorial_new(n):
+def factorial_new_new(n):
   if n == 0:
     return 1
   else:
-    return n * factorial_new(n - 1)
-print(factorial_new(5))
+    return n * factorial_new_new(n - 1)
+print(factorial_new_new(5))
