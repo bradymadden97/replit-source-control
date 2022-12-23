acf593eae10b8053399a4eb2c4ecc3be3ad113cd
Brady Madden <bradymadden97@gmail.com>
2022-12-23 18:08:12 -0500

    adding a new commit another commit, with sapling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def factorial_new_main(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial_new_main(n - 1)
-print(factorial_new_main(5))
+def factorial_new_main_toooo(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial_new_main_toooo(n - 1)
+
+
+print(factorial_new_main_toooo(5))
